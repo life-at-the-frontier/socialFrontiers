67f6fa84717ee38122a0679945994f519e0ca01d
Mengle Zhang <mi1mz@sheffield.ac.uk>
2020-04-15 14:51:29 +0100

    Made first function called startMyProject to
    1) load packages and their name space using #' @import from roxygen2 -- this is not part of the package but simply works when we use library(socialFrontiers)
    
    2) The function startMyProject itself loads data paths into the global variables uysing
    x <<- 'into global'
    
    <<- directly puts into global but be careful

---
+++ R/startMyProject.R
@@ -0,0 +1,18 @@
+#' The very first function in the entire project that loads in where the
+#'  data is and packages that we need
+
+#' @import tidyverse
+#' @import sf
+
+
+##  Data repo base location (e.g. google drive)
+
+startMyProject <-
+  function(
+  ){
+
+  ##  Assign file paths out of this function to global variables using <<-
+  dataBase_path <<-
+    'C:/Users/mi1mz/Google Drive/Google Drive/NordF project/Data'
+
+  }
