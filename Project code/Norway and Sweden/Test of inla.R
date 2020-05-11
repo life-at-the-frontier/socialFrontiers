328d1ff7ace56e1acd3a676fc146e598c2341fb8
Mengle Zhang <mi1mz@sheffield.ac.uk>
2020-05-11 13:57:23 +0100

    Tested/ changed/ verified frontier_detect work
    Major:
    1) Changed test script to reflect new package
    2) using the spdep:: for functions in frontier_detect to just simply get it to work without loading spdep (the binomial localkised inla function has too many calls to change atm)
    3) ran devtools::document() to generate documentation
    
    Issue: Still cannot get dependancies to load correctly.

--- Project code/Norway and Sweden/Test of inla.R
+++ Project code/Norway and Sweden/Test of inla.R
@@ -1,8 +1,11 @@
 ##  Example code using London and working with the routine
 ##  We had previously made function called inla_frontier which made the
 ##  binomial ina routine more usable
+##  delete global environment and do a clean and rebuild for this package
 
+startMyProject()
 library(INLA) ## for some reason I still need to call this EVEN though we have @import INLA in the function call
+library(socialFrontiers)
 
 ##  Load the data from the package namespace using data()
 data(london)# the london data should be in the name space
@@ -15,89 +18,20 @@ barnet <-
 ##  For binomial_inla we need to specify y (counts) and trials as well as contingency
 ##  matrix W
 
-y <- barnet$nonUK # 'nonUK' # Number of foreign
-n.trials <- barnet$totalPop #total population (per zone?)
+y <- 'nonUK' # 'nonUK' # Number of foreign
+n.trials <- 'totalPop' #total population (per zone?)
 
 barnet_sp <- barnet %>% as('Spatial')
 
-W.nb <- spdep::poly2nb(barnet_sp,
-                  queen = F, #more than just a single point touching to be neighbours
-                  row.names = 1:nrow(barnet_sp)) #Sadly we have to convert the sf object to spatial dataframe using as() here
-
-W <- spdep::nb2mat(W.nb, style = "B") # B = binary
-
 ##  Now we just run
-model <-
-  binomial_localisedINLA(y ~ 1, W = W, Ntrials = n.trials)
 
+model <-
+  frontier_detect(data = barnet, y = y, n.trials = n.trials)
+
+
+class(model) # correct
 
 ##  Based on my example code which used inla_frontier to preclean for binomial_localisedINLA
-# source('Source/inla_frontier.R')
 # source('Source/summary_frontier_model.R')
-
-
-##  My old code function
-##  Output:
-##  list object of class: frontier_model
-##
-
-# inla_frontier <- function(y, data, n.trials, W.nb = NULL, ...){
-#   ## Check format
-#   data.class <- class(data)
-#   if(!('SpatialPolygonsDataFrame' %in% data.class) & !('sf' %in% data.class)) stop ('Data not sf or SpatialPolygons')
-#
-#   if('sf' %in% class(data)){data2 <- data %>% as('Spatial')} else {data2 <- data}
-#
-#   ##  We do not touch data
-#   ##  We work with data2 and other versions copied (for just keep track of things)
-#
-#   ##  Creating the contiguity matrix: if no W.nb is supplied make one
-#   if(is.null(W.nb)){
-#     W.nb <- poly2nb(data2,
-#                     queen = F,#more than just a single point touching to be neighbours
-#                     row.names = 1:nrow(data2)) #Sadly we have to convert the sf object to spatial dataframe using as() here
-#   }
-#
-#   count.no.neighours <- sum(unlist(W.nb) == 0)
-#   if(count.no.neighours > 0){warning( count.no.neighours %>% paste('zone(s) have no neighbours!'))}
-#
-#   W <- nb2mat(W.nb, style = "B") # B = binary
-#
-#
-#   ##  INLA routine
-#   source("Source/binomial.localisedINLA.R") ## This needs to be loaded
-#
-#   x <- proc.time()
-#
-#   mod.inla <- binomial.localisedINLA(
-#     formula = data2[[y]] ~ 1,# y is variable name for number of foreigners
-#     W = W,
-#     Ntrials = data2[[n.trials]],# how many lived in zone
-#     fix.rho = TRUE,
-#     rho = 0.99
-#   )
-#
-#   print(proc.time() - x)
-#
-#
-#
-#   ##  So the inla output is just a list not a proper class object..
-#   mod.inla$W.frontiers <- mod.inla$W.estimated #extract estimated matrix
-#   # So basically we are putting NAs where there was no neighbour in the original
-#   # W matrix. Remainder are geographical neighbours with 0s and 1s denoting
-#   # sig correlation or not
-#   mod.inla$W.frontiers[W == 0] <- NA ## Put NAs where there was orginally not a border
-#   mod.inla$W.frontiers[lower.tri(mod.inla$W.frontiers, diag = T)] <- NA ## gets rid of the upper part of the sym. matrix
-#
-#   class(mod.inla) <- 'frontier_model' #changes it's class allowing for custom routines
-#
-#   return(mod.inla)
-# }
-#
-
-
-
-##  frontier_model <-
-##  inla_frontier(data = tower.sf, y = 'nonUK', n.trials = 'totalPop')
 
 
--- R/frontier_detect.R
+++ R/frontier_detect.R
@@ -1,54 +1,55 @@
 #' This is a wrapper for our frontier detection functions
 #' goal: standardise input and outputs regardless of technique?
-#' TODO: Check out how packages like matchit and synthpop deal with calling 
+#' TODO: Check out how packages like matchit and synthpop deal with calling
 #' methods from other packages
 #'  Note: orginally called inla_frontier in the project
 #'  Returns list object of class: frontier_model
 ##
 ##' @import INLA
+##' @import spdep
 
 
 frontier_detect <-
   function(y, data, n.trials, W.nb = NULL, ...) {
-    
+
     ## Initial check of format; data needs to be spatialpolygon or sf
     data.class <- class(data)
-    
+
     if (!('SpatialPolygonsDataFrame' %in% data.class) &
         !('sf' %in% data.class))
       stop ('Data class not sf or SpatialPolygons')
-    
+
     if ('sf' %in% class(data)) {
       data2 <- data %>% as('Spatial')
     } else {
       data2 <- data
     }
-    
+
     ##  We do not touch data
     ##  We work with data2 and other versions copied (for just keep track of things)
-    
+
     ##  Creating the contiguity matrix: if no W.nb is supplied make one
     if (is.null(W.nb)) {
-      W.nb <- poly2nb(data2,
+      W.nb <- spdep::poly2nb(data2,
                       queen = F,
                       #more than just a single point touching to be neighbours
                       row.names = 1:nrow(data2)) #Sadly we have to convert the sf object to spatial dataframe using as() here
     }
-    
+
     ## data check
     count.no.neighours <- sum(unlist(W.nb) == 0)
     if (count.no.neighours > 0) {
       warning(count.no.neighours %>% paste('zone(s) have no neighbours!'))
     }
-    
-    W <- nb2mat(W.nb, style = "B") # B = binary
-    
-    
+
+    W <- spdep::nb2mat(W.nb, style = "B") # B = binary
+
+
     ##  INLA routine
 #    source("Source/binomial.localisedINLA.R") ## This needs to be loaded
-    
+
     x <- proc.time()
-    
+
     mod.inla <- binomial_localisedINLA(
       formula = data2[[y]] ~ 1, ## y is variable name
       W = W,
@@ -56,11 +57,11 @@ frontier_detect <-
       fix.rho = TRUE,
       rho = 0.99
     )
-    
+
     print(proc.time() - x)
-    
-    
-    
+
+
+
     ##  So the inla output is just a list not a proper class object..
     mod.inla$W.frontiers <-
       mod.inla$W.estimated #extract estimated matrix
@@ -71,9 +72,9 @@ frontier_detect <-
       NA ## Put NAs where there was orginally not a border
     mod.inla$W.frontiers[lower.tri(mod.inla$W.frontiers, diag = T)] <-
       NA ## gets rid of the upper part of the sym. matrix
-    
+
     class(mod.inla) <-
       'frontier_model' #changes it's class allowing for custom routines
-    
+
     return(mod.inla)
   }
