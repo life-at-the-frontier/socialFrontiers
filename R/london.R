2a63b03baf7fe0c4e69022776411eb6a9d86a7ba
Mengle Zhang <mi1mz@sheffield.ac.uk>
2020-05-13 13:54:28 +0100

    Documentation for the london data created by Dan
    Placeholder since I haven't fully checked Dan's source code for compiling this. I suspect it's a mix of census and police.co.uk

---
+++ R/london.R
@@ -0,0 +1,22 @@
+#' @title London data [Placeholder]
+#'
+#' @description
+#' This is the data from London as an sf object with spatial polygons. Each record
+#' denotes a lower super output area (LSOA) in London.
+#'
+#' @details
+#' Originally sourced and formatted by Dan Olner.
+#'
+#' @format LSOA:
+#' Areal unit code
+#'
+#'@format LSOAname Areal unit name. The first part denotes the local authority (e.g. Barnet).
+#'@format UK Total count of UK born
+#'@format nonUK Total count of non-UK born
+#'@format totalPop Total resident population in LSOA
+#'@format econActive Total resident economically active (not necessarily employed)
+#'@format unemployed Total residents unemployed
+#'@format totalcrimes Total crime count from police.co.uk data
+#'
+
+"london"
